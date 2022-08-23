b2d39810322186a759f072308fd4829536dee312
Alexandr Radenko <ufodriverfromhell@gmail.com>
2022-08-23 17:09:33 +0300

    cycle-added

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-print('Hey boy!')
-name = input('Say my name! ')
-if name == 'Mr. White':
-    print('You goddam right!')
-else:
-    print(name, 'Is not my name bitch!')
+While True:
+    print('Hey boy!')
+    name = input('Say my name! ')
+    if name == 'Mr. White':
+        print('You goddam right!')
+    else:
+        print(name, 'Is not my name bitch!')
