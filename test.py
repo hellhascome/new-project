c26756f718d47ee21d49bbc543242c5b012e5fd9
Alexandr Radenko <ufodriverfromhell@gmail.com>
2022-08-23 17:17:40 +0300

    cycle-fixed

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-While True:
+while True:
     print('Hey boy!')
     name = input('Say my name! ')
     if name == 'Mr. White':
         print('You goddam right!')
+        break
     else:
         print(name, 'Is not my name bitch!')
